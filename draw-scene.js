75b0cf70a8c28ca2f8fd5ee5e3f8e29529629d1a
ice-cube-1 <alice.goward@spgs.org>
2024-07-13 10:57:28 +0100

    some procedurally generated stuff + added libs to local src code

--- draw-scene.js
+++ draw-scene.js
@@ -1,18 +1,18 @@
 function drawScene(gl, programInfo, buffers, floortexture, walltexture, sword, cameraRotationX, cameraRotationY, xpos, ypos, zpos, items, attackPos, players, character) {
-    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearColor(0.8, 0.9, 1.0, 1.0);
     gl.clearDepth(1.0);
     gl.enable(gl.DEPTH_TEST);
     gl.depthFunc(gl.LEQUAL);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    const fieldOfView = (45*Math.PI)/180;
-    const aspect = gl.canvas.clientWidth/gl.canvas.clientHeight;
+    const fieldOfView = (45 * Math.PI) / 180;
+    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
     const zNear = 0.1;
     const zFar = 100.0;
     const projectionMatrix = mat4.create();
     mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
     const modelViewMatrix = mat4.create();
-    mat4.rotate(modelViewMatrix, modelViewMatrix, cameraRotationY*1.5, [1,0,0])
-    mat4.rotate(modelViewMatrix, modelViewMatrix, cameraRotationX*4, [0,1,0])
+    mat4.rotate(modelViewMatrix, modelViewMatrix, cameraRotationY * 1.5, [1, 0, 0])
+    mat4.rotate(modelViewMatrix, modelViewMatrix, cameraRotationX * 4, [0, 1, 0])
     mat4.translate(modelViewMatrix, modelViewMatrix, [xpos, -zpos, ypos],);
     const normalMatrix = mat4.create();
     mat4.invert(normalMatrix, modelViewMatrix);
@@ -32,7 +32,7 @@ function drawScene(gl, programInfo, buffers, floortexture, walltexture, sword, c
     const offset = 0;
     for (let i = 0; i < items.length; i++) {
         if (items[i][1] == 0) { gl.bindTexture(gl.TEXTURE_2D, floortexture); }
-        else { gl.bindTexture(gl.TEXTURE_2D, walltexture)}
+        else { gl.bindTexture(gl.TEXTURE_2D, walltexture) }
         mat4.translate(modelViewMatrix, modelViewMatrix, items[i]);
         gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
         gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
@@ -40,19 +40,19 @@ function drawScene(gl, programInfo, buffers, floortexture, walltexture, sword, c
     }
     gl.bindTexture(gl.TEXTURE_2D, character)
     for (let i = 0; i < players.length; i++) {
-        mat4.translate(modelViewMatrix, modelViewMatrix,  [players[i].x, players[i].z-2, players[i].y]);
-        mat4.scale(modelViewMatrix, modelViewMatrix, [1/1.2, 2, 1/1.2])
-        mat4.rotate(modelViewMatrix, modelViewMatrix, players[i].rotation, [0,1,0])
+        mat4.translate(modelViewMatrix, modelViewMatrix, [players[i].x, players[i].z - 2, players[i].y]);
+        mat4.scale(modelViewMatrix, modelViewMatrix, [1 / 1.2, 2, 1 / 1.2])
+        mat4.rotate(modelViewMatrix, modelViewMatrix, players[i].rotation, [0, 1, 0])
         gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
         gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
-        mat4.rotate(modelViewMatrix, modelViewMatrix, -players[i].rotation, [0,1,0])
+        mat4.rotate(modelViewMatrix, modelViewMatrix, -players[i].rotation, [0, 1, 0])
         mat4.scale(modelViewMatrix, modelViewMatrix, [1.2, 0.5, 1.2])
-        mat4.translate(modelViewMatrix, modelViewMatrix, [-players[i].x, -players[i].z+2, -players[i].y]);
+        mat4.translate(modelViewMatrix, modelViewMatrix, [-players[i].x, -players[i].z + 2, -players[i].y]);
     }
     gl.bindTexture(gl.TEXTURE_2D, sword);
     const fixedModelViewMatrix = mat4.create();
     mat4.translate(fixedModelViewMatrix, fixedModelViewMatrix, [-1, 0, -1]);
-    mat4.rotate(fixedModelViewMatrix, fixedModelViewMatrix, attackPos*4, [1,0,0])
+    mat4.rotate(fixedModelViewMatrix, fixedModelViewMatrix, attackPos * 4, [1, 0, 0])
     mat4.scale(fixedModelViewMatrix, fixedModelViewMatrix, [0.01, 1, 0.2]);
     gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, fixedModelViewMatrix);
     gl.drawElements(gl.TRIANGLES, vertexCount, type, offset);
@@ -72,7 +72,7 @@ function setPositionAttribute(gl, buffers, programInfo) {
         normalize,
         stride,
         offset,
-      );
+    );
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
 }
 
--- webgl-demo.js
+++ webgl-demo.js
@@ -2,25 +2,51 @@ import { initBuffers } from "./init-buffers.js";
 import { drawScene } from "./draw-scene.js";
 
 const hitbox = 0.5;
-const items = [[-4,4,-4],[4,4,-4],[-4,4,4],[4,4,4],
-                [-2,2,-4],[-4,2,-4],[2,2,-4],[4,2,-4],
-                [-2,2,-2],[-4,2,-2],[2,2,-2],[4,2,-2],
-                [-2,2,2],[-4,2,2],[2,2,2],[4,2,2],
-                [-2,2,4],[-4,2,4],[2,2,4],[4,2,4]];
-for (var i = -20; i<=20; i+=2) {
-    for (var j = -20; j<=20; j+=2) {
-        items.push([i, 0, j])
-    }
-    items.push([i,2,-20])
-    items.push([i,2,20])
-    items.push([i,4,-20])
-    items.push([i,4,20])
-    items.push([-20,2,i])
-    items.push([20,2,i])
-    items.push([-20,4,i])
-    items.push([20,4,i])
+
+function addmore(i,z,j) {
+    if (Math.random() < 0.2) {
+        items.push([i,z+2,j]);
+        addmore(i,z+2,j)
+    } 
 }
-var players = [{x: 0, y: 10, z: 5, rotation: 0, zspeed: 0}, {x: 10, y: 10, z: 5, rotation: 0.5, zspeed: 0}, {x: -10, y: 0, z: 5, rotation: -1, zspeed:0}]
+
+const gridsize = 80
+var items = []
+for (var i = -gridsize/2; i< gridsize/2; i+=2) {
+    for (var j = -gridsize/2; j< gridsize/2; j+=2) {
+        items.push([i,0,j]);
+        console.log(items)
+        addmore(i,0,j)
+    }
+    items.push([i,2,gridsize/2])
+    items.push([i,4,gridsize/2])
+    items.push([gridsize/2, 2, i])
+    items.push([gridsize/2, 4, i])
+    items.push([i,2,-gridsize/2])
+    items.push([i,4,-gridsize/2])
+    items.push([-gridsize/2, 2, i])
+    items.push([-gridsize/2, 4, i])
+}
+
+// const items = [[-4, 4, -4], [4, 4, -4], [-4, 4, 4], [4, 4, 4],
+// [-2, 2, -4], [-4, 2, -4], [2, 2, -4], [4, 2, -4],
+// [-2, 2, -2], [-4, 2, -2], [2, 2, -2], [4, 2, -2],
+// [-2, 2, 2], [-4, 2, 2], [2, 2, 2], [4, 2, 2],
+// [-2, 2, 4], [-4, 2, 4], [2, 2, 4], [4, 2, 4]];
+// for (var i = -20; i <= 20; i += 2) { 
+//     for (var j = -20; j <= 20; j += 2) {
+//         items.push([i, 0, j])
+//     }
+//     items.push([i, 2, -20])
+//     items.push([i, 2, 20])
+//     items.push([i, 4, -20])
+//     items.push([i, 4, 20])
+//     items.push([-20, 2, i])
+//     items.push([20, 2, i])
+//     items.push([-20, 4, i])
+//     items.push([20, 4, i])
+// }
+var players = [{ x: 0, y: 10, z: 5, rotation: 0, zspeed: 0 }, { x: 10, y: 10, z: 5, rotation: 0.5, zspeed: 0 }, { x: -10, y: 0, z: 5, rotation: -1, zspeed: 0 }]
 
 let Xpos = 0;
 let Ypos = 0;
@@ -71,17 +97,17 @@ function main() {
     const programInfo = {
         program: shaderProgram,
         attribLocations: {
-          vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-          vertexNormal: gl.getAttribLocation(shaderProgram, "aVertexNormal"),
-          textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
+            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+            vertexNormal: gl.getAttribLocation(shaderProgram, "aVertexNormal"),
+            textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
         },
         uniformLocations: {
-          projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
-          modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
-          normalMatrix: gl.getUniformLocation(shaderProgram, "uNormalMatrix"),
-          uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
+            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+            normalMatrix: gl.getUniformLocation(shaderProgram, "uNormalMatrix"),
+            uSampler: gl.getUniformLocation(shaderProgram, "uSampler"),
         },
-      };            
+    };
     const buffers = initBuffers(gl);
     const floortexture = loadTexture(gl, "floortexture.png");
     const walltexture = loadTexture(gl, "walltexture.png")
@@ -89,19 +115,19 @@ function main() {
     const character = loadTexture(gl, "character.png")
     gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
     function render() {
-        document.getElementById("coordinates").textContent = `X: ${Math.round(Xpos)}, Y: ${Math.round(Ypos)}, Z: ${Math.floor((Zpos-4)/2)}`;
+        document.getElementById("coordinates").textContent = `X: ${Xpos.toFixed(2)}, Y: ${Ypos.toFixed(2)}, Z: ${Math.floor((Zpos - 4) / 2)}`;
         document.getElementById("chat").innerHTML = messages.join("<br/>");
         console.log(messages.join("\n"))
-        Zpos+=zspeed/10
-        attackPos-=attackSpeed
+        Zpos += zspeed / 10
+        attackPos -= attackSpeed
         if (attackPos <= 0.7) {
             attackPos = 1.6;
             attackSpeed = 0;
         }
-        Zpos, zspeed = gravity(Xpos,Ypos,Zpos,zspeed)
-        for (var i = 0; i<players.length; i++) {
-            players[i].z+=players[i].zspeed/10
-            players[i].z, players[i].zspeed = gravity(players[i].z, players[i].y, players[i].z,players[i].zspeed)
+        Zpos, zspeed = gravity(Xpos, Ypos, Zpos, zspeed)
+        for (var i = 0; i < players.length; i++) {
+            players[i].z += players[i].zspeed / 10
+            players[i].z, players[i].zspeed = gravity(players[i].z, players[i].y, players[i].z, players[i].zspeed)
         }
         drawScene(gl, programInfo, buffers, floortexture, walltexture, weapontexture, mousePos.x, mousePos.y, Xpos, Ypos, Zpos, items, attackPos, players, character);
         requestAnimationFrame(render);
@@ -115,12 +141,12 @@ function main() {
     addEventListener("click", (event) => {
         if (attackSpeed == 0) {
             attackSpeed = 0.02
-            for (var i = 0; i<players.length; i++) {
-                if ((-Xpos-players[i].x)**2 + (-Ypos-players[i].y)**2 <= (hitbox*8)**2 && (Math.abs(Zpos-players[i].z) < 4)) {
-                    players[i].zspeed+=1
-                    let vec = {x: -Math.sin(mousePos.x*4), y: Math.cos(mousePos.x*4)}
-                    var tempX = players[i].x-vec.x * speed;
-                    var tempY = players[i].y-vec.y * speed;
+            for (var i = 0; i < players.length; i++) {
+                if ((-Xpos - players[i].x) ** 2 + (-Ypos - players[i].y) ** 2 <= (hitbox * 8) ** 2 && (Math.abs(Zpos - players[i].z) < 4)) {
+                    players[i].zspeed += 1
+                    let vec = { x: -Math.sin(mousePos.x * 4), y: Math.cos(mousePos.x * 4) }
+                    var tempX = players[i].x - vec.x * speed;
+                    var tempY = players[i].y - vec.y * speed;
                     if (checkNotCollision(tempX, tempY)) {
                         players[i].x = tempX;
                         players[i].y = tempY;
@@ -130,38 +156,38 @@ function main() {
         }
     });
     $(document).keydown(function (e) {
-        let rightVector = { x: Math.cos(mousePos.x*4), y: Math.sin(mousePos.x*4) };
-        let forwardVector = { x: -Math.sin(mousePos.x*4), y: Math.cos(mousePos.x*4) };
+        let rightVector = { x: Math.cos(mousePos.x * 4), y: Math.sin(mousePos.x * 4) };
+        let forwardVector = { x: -Math.sin(mousePos.x * 4), y: Math.cos(mousePos.x * 4) };
         if (!chatFocussed) {
-        switch (e.which) {
+            switch (e.which) {
                 case 87: // W key - Move forward
-                    var tempX = Xpos+forwardVector.x * speed;
-                    var tempY = Ypos+forwardVector.y * speed;
-                    if (checkNotCollision(tempX, tempY) && playerPlayerCollision(tempX,tempY,Zpos)) {
+                    var tempX = Xpos + forwardVector.x * speed;
+                    var tempY = Ypos + forwardVector.y * speed;
+                    if (checkNotCollision(tempX, tempY) && playerPlayerCollision(tempX, tempY, Zpos)) {
                         Xpos = tempX;
                         Ypos = tempY
                     }
                     break;
                 case 65: // A key - Move left
-                    var tempX = Xpos+rightVector.x * speed;
-                    var tempY = Ypos+rightVector.y * speed;
-                    if (checkNotCollision(tempX, tempY) && playerPlayerCollision(tempX,tempY,Zpos)) {
+                    var tempX = Xpos + rightVector.x * speed;
+                    var tempY = Ypos + rightVector.y * speed;
+                    if (checkNotCollision(tempX, tempY) && playerPlayerCollision(tempX, tempY, Zpos)) {
                         Xpos = tempX;
                         Ypos = tempY
                     }
                     break;
                 case 83: // S key - Move backward
-                    var tempX = Xpos-forwardVector.x * speed;
-                    var tempY = Ypos-forwardVector.y * speed;
-                    if (checkNotCollision(tempX, tempY) && playerPlayerCollision(tempX,tempY,Zpos)) {
+                    var tempX = Xpos - forwardVector.x * speed;
+                    var tempY = Ypos - forwardVector.y * speed;
+                    if (checkNotCollision(tempX, tempY) && playerPlayerCollision(tempX, tempY, Zpos)) {
                         Xpos = tempX;
                         Ypos = tempY
                     }
                     break;
                 case 68: // D key - Move right
-                    var tempX = Xpos-rightVector.x * speed;
-                    var tempY = Ypos-rightVector.y * speed;
-                    if (checkNotCollision(tempX, tempY) && playerPlayerCollision(tempX,tempY,Zpos)) {
+                    var tempX = Xpos - rightVector.x * speed;
+                    var tempY = Ypos - rightVector.y * speed;
+                    if (checkNotCollision(tempX, tempY) && playerPlayerCollision(tempX, tempY, Zpos)) {
                         Xpos = tempX;
                         Ypos = tempY
                     }
@@ -175,25 +201,25 @@ function main() {
                     chatFocussed = true;
                     messages.push("")
                     if (messages.length >= 3) {
-                        messages = messages.slice(messages.length-3,messages.length)
+                        messages = messages.slice(messages.length - 3, messages.length)
                     }
             }
         } else {
             if (e.which == 13) {
                 chatFocussed = false;
             } else if (e.which == 8) {
-                messages[messages.length-1] = messages[messages.length-1].substring(0, messages[messages.length-1].length - 1);
+                messages[messages.length - 1] = messages[messages.length - 1].substring(0, messages[messages.length - 1].length - 1);
             } else {
-                messages[messages.length-1] += String.fromCharCode(e.which).toLowerCase()
+                messages[messages.length - 1] += String.fromCharCode(e.which).toLowerCase()
             }
         }
     });
 }
 
-function playerPlayerCollision(x,y,z) {
-    for (var i = 0; i<players.length; i++) {
-        if (Math.abs(z-players[i].z) < 4) {
-            if ((-x-players[i].x)**2 + (-y-players[i].y)**2 <= (hitbox*4)**2) {
+function playerPlayerCollision(x, y, z) {
+    for (var i = 0; i < players.length; i++) {
+        if (Math.abs(z - players[i].z) < 4) {
+            if ((-x - players[i].x) ** 2 + (-y - players[i].y) ** 2 <= (hitbox * 4) ** 2) {
                 return false
             }
         }
@@ -201,16 +227,16 @@ function playerPlayerCollision(x,y,z) {
     return true
 }
 
-function checkNotCollision(playerX,playerY) {
+function checkNotCollision(playerX, playerY) {
     for (var i = 0; i < items.length; i++) {
         if (items[i][1] <= Math.ceil(Zpos) && items[i][1] >= Math.ceil(Zpos) - 3) {
-            var circleDistance = {x: Math.abs(items[i][0] + playerX), y: Math.abs(items[i][2] + playerY)}
+            var circleDistance = { x: Math.abs(items[i][0] + playerX), y: Math.abs(items[i][2] + playerY) }
             if (circleDistance.x > (1 + hitbox)) { continue; }
             if (circleDistance.y > (1 + hitbox)) { continue; }
-            if (circleDistance.x <= 1) { return false; } 
+            if (circleDistance.x <= 1) { return false; }
             if (circleDistance.y <= 1) { return false; }
-            var cornerDistance_sq = (circleDistance.x - 1)**2 + (circleDistance.y - 1)**2;
-            if (cornerDistance_sq <= (hitbox**2)) {return false}
+            var cornerDistance_sq = (circleDistance.x - 1) ** 2 + (circleDistance.y - 1) ** 2;
+            if (cornerDistance_sq <= (hitbox ** 2)) { return false }
         }
     }
     return true
@@ -243,14 +269,14 @@ function loadTexture(gl, url) {
     const border = 0;
     const srcFormat = gl.RGBA;
     const srcType = gl.UNSIGNED_BYTE;
-    const pixel = new Uint8Array([0,0,255,255]);
+    const pixel = new Uint8Array([0, 0, 255, 255]);
     gl.blendFunc(gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA);
     gl.enable(gl.BLEND);
     gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, width, height, border, srcFormat, srcType, pixel);
     const image = new Image();
     image.onload = () => {
-            gl.bindTexture(gl.TEXTURE_2D, texture);
-            gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
+        gl.bindTexture(gl.TEXTURE_2D, texture);
+        gl.texImage2D(gl.TEXTURE_2D, level, internalFormat, srcFormat, srcType, image);
         if (isPowerOf2(image.width) && isPowerOf2(image.height)) {
             gl.generateMipmap(gl.TEXTURE_2D)
         } else {
@@ -265,24 +291,24 @@ function loadTexture(gl, url) {
 
 function isPowerOf2(value) {
     return (value & (value - 1)) === 0;
-  }
+}
 
-  function getMousePosition(event, target) {
+function getMousePosition(event, target) {
     target = target || event.target;
     const rect = target.getBoundingClientRect();
     const x = (event.clientX - rect.left) / rect.width * 2 - 1;
     const y = (event.clientY - rect.top) / rect.height * 2 - 1;
     return { x, y };
 }
-function gravity(Xpos,Ypos,Zpos,zspeed) {
-    var floor = [Math.round(Xpos / 2) * 2, Math.ceil(Zpos - 5), Math.ceil(Ypos / 2) * 2];
+function gravity(Xpos, Ypos, Zpos, zspeed) {
+    var floor = [Math.round(-Xpos / 2) * 2, Math.ceil(Zpos - 5), Math.ceil(-Ypos / 2) * 2];
     for (var i = 0; i < items.length; i++) {
         if (items[i][0] === floor[0] && items[i][1] === floor[1] && items[i][2] === floor[2]) {
             zspeed = 0;
             Zpos = Math.ceil(Zpos);
             return Zpos, zspeed;
         }
-    } 
+    }
     zspeed -= 0.08;
     return Zpos, zspeed;
 }
